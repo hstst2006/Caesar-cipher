9a7e7672736884b1c93a0983ae88e2f35bfd8fb9
Håvard Skordal Thorsen <haavardsth@uia.no>
2020-10-10 20:08:45 +0200

    Changed the encrypt and decrypt functions to work with unsigned integers instead of signed integers to account for values above 128. Added readme file.

--- main.c
+++ main.c
@@ -1,11 +1,3 @@
-//This program encrypts your input using the Caesar cipher method of shifting the letter number by a predefined number.
-//Input a number for the cipher and a message to encrypt
-//Input a number for the cipher and a message to decrypt
-//Ascii numbers for uppercase letters: 65-90
-//Ascii numbers for lowercase letters: 97-122
-//If a letter is shifted outside the number range (ex 122 + 5 = 127) subtract 57.
-//If number is from 91 - 96 shift up to 97 (if using a positive number) or down to 90 (if using a negative number)
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -28,7 +20,6 @@ void menu(char array[LIMIT]){
             case 2:
                 system("cls"); //Clears the system terminal (in windows)
                 decryptString(array, cipherKey());
-                system("cls");
                 break;
             case 3:
                 exit(0);
