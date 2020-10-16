dfbdae6bc33e9868412658de2fa38c0d90660d16
Håvard Skordal Thorsen <haavardsth@uia.no>
2020-10-16 02:53:12 +0200

    Removed the function setCharLimit from main, it now only exists in "menus".

--- main.c
+++ main.c
@@ -4,14 +4,6 @@
 #include "encrypt.h"
 #include "menus.h"
 
-
-void setCharLimit(int* charLimit){
-    printf("Please enter a character limit: ");
-    scanf("%i", charLimit);
-}
-
-
-
 int main() {
     //int charLimit = 0;
     //setCharLimit(&charLimit);
